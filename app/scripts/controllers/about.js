27163b138e6e07951ef3d5199385f043e0af12f0
Greg Weisman <grwgreg@gmail.com>
2015-09-02 18:07:05 -0400

    yeoman angular scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mymovieApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mymovieApp
+ */
+angular.module('mymovieApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mymovieApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mymovieApp
+ */
+angular.module('mymovieApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

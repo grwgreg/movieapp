e32a46e5aa92b7109e402261ddfce28f3d29bef2
Greg Weisman <grwgreg@gmail.com>
2015-09-04 19:08:07 -0400

    ui bootstrap and pagination directive added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,26 @@
 angular.module('mymovieApp')
   .controller('MainCtrl', ['$scope', 'movieData', '$log', function($scope, movieData, $log) {
     var vm = this;
+    
+    vm.loading = true;
+    vm.page = 1;//route param maybe?
+
+    vm.onPagination = function() {
+console.log('page ', this.page);
+    };
+
+setInterval(function() {
+          $scope.$apply(function() {
+            vm.page++;
+          });
+}, 8000);
 
     movieData.get().success(function(data) {
         console.log('yes', data);
         setTimeout(function(){
           $scope.$apply(function() {
             vm.results = data.results
+            vm.loading = false;
           });
         },5000);//simulate slow load
       })

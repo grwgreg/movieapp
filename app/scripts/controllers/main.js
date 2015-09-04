31324f7bd8137b0e3e991150ca82b7b9f5899de9
Greg Weisman <grwgreg@gmail.com>
2015-09-04 19:29:42 -0400

    working pagination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,29 +13,38 @@ angular.module('mymovieApp')
     
     vm.loading = true;
     vm.page = 1;//route param maybe?
+    vm.totalItems = 0;
+    vm.results = [];
 
     vm.onPagination = function() {
-console.log('page ', this.page);
+      console.log('page ', vm.page);
+      vm.loading = true;
+      vm.results = [];
+      movieData.get(vm.page).success(function(data) {
+          console.log('yes', data);
+          setTimeout(function(){
+            $scope.$apply(function() {
+              vm.results = data.results
+              vm.loading = false;
+              vm.totalItems = data.total_results;
+            });
+          },1000);//simulate slow load
+        })
+        .error(function(e) {
+          $log.error(e);
+        });
     };
 
+   vm.onPagination();//initial load of data
+
+/*
 setInterval(function() {
           $scope.$apply(function() {
             vm.page++;
           });
 }, 8000);
+*/
 
-    movieData.get().success(function(data) {
-        console.log('yes', data);
-        setTimeout(function(){
-          $scope.$apply(function() {
-            vm.results = data.results
-            vm.loading = false;
-          });
-        },5000);//simulate slow load
-      })
-      .error(function(e) {
-        $log.error(e);
-      });
 
 
   }]);
--- app/scripts/services/moviedata.js
+++ app/scripts/services/moviedata.js
@@ -1,8 +1,9 @@
 angular.module('mymovieApp')
   .service('movieData', ['$http', 'TMDB_KEY', 'TMDB_URL', function($http, TMDB_KEY, TMDB_URL) {
     return {
-      get: function() {
-        return $http.get(TMDB_URL + "discover/movie?sort_by=popularity.desc&api_key=" + TMDB_KEY)
+      get: function(page) {
+        //https://developers.themoviedb.org/3/discover
+        return $http.get(TMDB_URL + "discover/movie?sort_by=popularity.desc&page=" + page + "&api_key=" + TMDB_KEY)
       }
     };
   }]);
